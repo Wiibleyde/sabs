52f44f75bdd4fd2b405bd4b12b7f3759e301af80
Wiibleyde <nathan@bonnell.fr>
2026-06-04 10:43:57 +0200

    fix: add smooth scroll behavior to html element
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="fr">
+		<html lang="fr" data-scroll-behavior="smooth">
 			<head>
 				<meta
 					name="google-site-verification"
